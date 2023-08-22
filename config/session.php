807cbabec65344dcbcc609b4ac7da907bacaa10e
AlgonzaDJun <algonsadewangga@gmail.com>
2023-08-23 00:10:17 +0700

    laravel 8

--- config/session.php
+++ config/session.php
@@ -125,8 +125,7 @@ return [
     */
 
     'cookie' => env(
-        'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+        'SESSION_COOKIE', Str::slug(env('APP_NAME', 'laravel'), '_') . '_session'
     ),
 
     /*
@@ -166,7 +165,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', false),
+    'secure' => env('SESSION_SECURE_COOKIE', null),
 
     /*
     |--------------------------------------------------------------------------
--- routes/web.php
+++ routes/web.php
@@ -12,28 +12,39 @@
 */
 
 // use App\Http\Controllers\Admin\TravelPackageController;
+
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\GalleryController;
+use App\Http\Controllers\Admin\TransactionController;
+use App\Http\Controllers\Admin\TravelPackageController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\DetailController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MidtransController;
+use App\Http\Controllers\OrdersController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-Route::get('/', 'HomeController@index')
+Route::get('/', [HomeController::class, 'index'])
     ->name('home');
 
-Route::get('/detail/{slug}', 'DetailController@index')
+Route::get('/detail/{slug}', [DetailController::class, 'index'])
     ->name('detail');
 
-Route::post('/checkout/{id}', 'CheckoutController@process')
+Route::post('/checkout/{id}', [CheckoutController::class, 'process'])
     ->name('checkout-process')
     ->middleware(['auth', 'verified']);
 
-Route::get('/checkout/{id}', 'CheckoutController@index')
+Route::get('/checkout/{id}', [CheckoutController::class, 'index'])
     ->name('checkout')
     ->middleware(['auth', 'verified']);
 
-Route::post('/checkout/create/{detail_id}', 'CheckoutController@create')
+Route::post('/checkout/create/{detail_id}', [CheckoutController::class, 'create'])
     ->name('checkout-create')
     ->middleware(['auth', 'verified']);
 
-Route::post('/checkout/remove/{detail_id}', 'CheckoutController@remove')
+Route::post('/checkout/remove/{detail_id}', [CheckoutController::class, 'remove'])
     ->name('checkout-remove')
     ->middleware(['auth', 'verified']);
 
@@ -42,29 +53,29 @@ Route::post('/checkout/confirm/{id}', 'CheckoutController@success')
     ->middleware(['auth', 'verified']);
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
-    Route::get('/profile', 'ProfileController@index')
+    Route::get('/profile', [ProfileController::class, 'index'])
         ->name('profile');
-    Route::post('/profile', 'ProfileController@update')
+    Route::post('/profile', [ProfileController::class, 'update'])
         ->name('profile-update');
 });
 
-Route::get('/orders', 'OrdersController@index');
+Route::get('/orders', [OrdersController::class, 'index']);
 
 Route::prefix('admin')
     ->namespace('Admin')
     ->middleware(['auth', 'admin'])
     ->group(function () {
-        Route::get('/', 'DashboardController@index')
+        Route::get('/', [DashboardController::class, 'index'])
             ->name('dashboard');
-        Route::resource('travel-package', 'TravelPackageController');
-        Route::resource('gallery', 'GalleryController');
-        Route::resource('transaction', 'TransactionController');
+        Route::resource('travel-package', TravelPackageController::class);
+        Route::resource('gallery', GalleryController::class);
+        Route::resource('transaction', TransactionController::class);
     });
 
 Auth::routes(['verify' => true]);
 
 // midtrans
-Route::post('/midtrans/callback', 'MidtransController@notificationHandler');
-Route::get('/midtrans/finish', 'MidtransController@finishRedirect');
-Route::get('/midtrans/unfinished', 'MidtransController@unfinishedRedirect');
-Route::get('/midtrans/error', 'MidtransController@errorRedirect');
+Route::post('/midtrans/callback', [MidtransController::class, 'notificationHandler']);
+Route::get('/midtrans/finish', [MidtransController::class, 'finishRedirect']);
+Route::get('/midtrans/unfinished', [MidtransController::class, 'unfinishedRedirect']);
+Route::get('/midtrans/error', [MidtransController::class, 'errorRedirect']);
